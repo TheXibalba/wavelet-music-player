df76c07aa736ca852f7f2b0b56ac0784e1d6850d
yuvraj Pawar <yurajpawar99@gmail.com>
2020-11-20 21:35:03 +0530

    Initial skeleton build

---
+++ src/components/Song.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Song=()=>{
+    return(
+        <div className="song-container">
+
+<h1>Picture</h1>
+<h1>Song Name</h1>
+<h1>Artist</h1>
+        </div>
+
+    );
+}
+
+export default Song;
