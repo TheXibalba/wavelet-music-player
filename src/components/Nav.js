6ce5ff0895cbec0334df2a6577c81ea3f9c9baa2
yuvraj Pawar <yurajpawar99@gmail.com>
2020-11-22 11:53:25 +0530

    Minor changes. FINAL Build

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,15 +5,12 @@ import {faMusic} from "@fortawesome/free-solid-svg-icons";
 const Nav=({libraryStatus,setLibraryStatus})=>{
     return(
 <nav>
-<h1>Waves</h1>
+<h1>Wavelet</h1>
 <button onClick={()=>setLibraryStatus(!libraryStatus)} >
     Library
     <FontAwesomeIcon icon={faMusic}/>
 </button>
 </nav>
-
-
-
     );
 }
 
